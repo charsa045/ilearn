d929858a09411e982c87f59694fd351128a3769a
charsa045 <codexcharsa@gmail.com>
2026-05-25 19:29:51 -0600

    Update route.ts

--- app/api/docentes/route.ts
+++ app/api/docentes/route.ts
@@ -112,7 +112,7 @@ export async function POST(req: NextRequest) {
     const docente = await createDocente({
       uid: finalUid,
       nombre,
-      imageUrl, // ✅ ya no necesitas || ""
+      imageUrl,
       grado,
       titulo,
       especialidad,
